c03b73ebd0979c867e68bd4d4a38946b80ef5d31
KurtH75 <Kurt.Sidekick@yahoo.com>
2024-12-24 13:35:29 +0100

    f

--- esphome/components/vscp/light/__init__.py
+++ esphome/components/vscp/light/__init__.py
@@ -27,7 +27,7 @@ def to_code(config):
     yield light.register_light(var, config)
 
     canbus = yield cg.get_variable(config["canbus_id"])
-    cg.add(vscp.set_canbus(canbus))
+    cg.add(var.set_canbus(canbus))
 
     out = yield cg.get_variable(config[CONF_OUTPUT])
     cg.add(var.set_output(out))

3980f1c2680255fd41476220b4a5acbcf6783d95
KurtH75 <Kurt.Sidekick@yahoo.com>
2025-02-08 19:11:29 +0100

    q

--- esphome/components/vscp2/light/light.cpp
+++ esphome/components/vscp2/light/light.cpp
@@ -22,7 +22,7 @@ void Vscp2Light::setup_state(light::LightState *state) {
 	state_ = state;
 	state_->set_gamma_correct(0);
 	state_->set_default_transition_length(0);
-	state_->set_restore_mode((LightRestoreMode) 0); //LIGHT_RESTORE_DEFAULT_OFF
+	state_->set_restore_mode(LightRestoreMode 0); //LIGHT_RESTORE_DEFAULT_OFF
 } 
 
 void Vscp2Light::write_state(light::LightState *state) {

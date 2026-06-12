c9d04a55e7d9722dfdb73946781f7fdd75fff1bc
KurtH75 <Kurt.Herremans@yahoo.com>
2026-06-12 19:51:35 +0200

    aa

--- esphome/components/vscp2/vscp2.cpp
+++ esphome/components/vscp2/vscp2.cpp
@@ -80,7 +80,7 @@ void Vscp2Component::set_canbus(canbus::Canbus *canbus) {
 
 	canbus_canbustrigger = new canbus::CanbusTrigger(canbus, 0, 0, true); //permit all messages
 	canbus_canbustrigger->set_component_source(LOG_STR("canbus"));
-  App.register_component(canbus_canbustrigger);
+  App.register_component_(canbus_canbustrigger);
   automation = new Automation<std::vector<uint8_t>, uint32_t, bool>(canbus_canbustrigger);
   auto cb = [this](std::vector<uint8_t> x, uint32_t can_id, bool remote_transmission_request) -> void {
     this-> receive(can_id, x);
